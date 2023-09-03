d12a2ffc605fd998af2c7a81a3f2f6071c13b10e
FuzzyYeti <fuzzyyeti@users.noreply.github.com>
2023-09-02 19:42:59 -0600

    make tests pass

--- src/instruction-discriminator.ts
+++ src/instruction-discriminator.ts
@@ -58,7 +58,10 @@ export class InstructionDiscriminator {
     let ty: IdlType
     if(JSON.stringify(rawTy) === '"u8"'){
       ty = "u8"
-    } else if(JSON.stringify(rawTy) === '{"array":["u8",8]}'){
+    } else if (JSON.stringify(rawTy) === '"u32"'){
+      ty = "u32"
+    }
+    else if (JSON.stringify(rawTy) === '{"array":["u8",8]}'){
       ty = { array: ['u8', 8] } as IdlTypeArray
     } else {
       throw new Error(`Unsupported type ${rawTy} when parsing discriminant for instruction ${this.ix.name}`);
